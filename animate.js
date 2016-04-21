00fc0c11d099c4736dfcda32f22393bfb14b401e
GuyWithTheGlasses <kevin.a.yan@gmail.com>
2016-04-20 21:48:22 -0400

    Balls move around the canvas and bounce off walls

--- animate.js
+++ animate.js
@@ -3,14 +3,8 @@ console.log("animate");
 /*--------------------------- Initialization -----------------------------*/
 
 //Get the canvas and prep it for 2d drawing
-var c = document.getElementById("animator");
+var c = document.getElementById("playground");
 var ctx = c.getContext("2d");
-
-//Set color
-ctx.strokeStyle = "#60CFD0";
-ctx.fillStyle = "#60CFD0";
-
-var requestID; //To store the current animation frame ID
 
 /*------------------------ Random Number Generator ------------------------*/
 
@@ -36,28 +30,76 @@ var ball = function(startx,starty,radius){
     
     return {
 	getx : function(){ return x; },
-	gety : function(){ return y; ),
+	gety : function(){ return y; },
 	getr : function(){ return r; },
 
-	//We don't need set functions - we only need to move the ball's 
-	//position by its velocity, which we do with increment functions
-	incx : function(){ x += xvel; },
-	incy : function(){ y += yvel; },
+	//We don't need set functions - we only need to change the ball's 
+	//position by its velocity, which we can do with increment functions
+	incx : function(i){ x += i; },
+	incy : function(j){ y += j; },
 
 	getxv : function(){ return xvel; },
 	getyv : function(){ return yvel; },
 
-	//Velocities will never have a magnitude > 1 (hard to do collision)
-	//Therefore we only need to negate them when the ball bounces
-	negxv : function(){ xvel *= -1; },
-	negyv : function(){ yvel *= -1; },
+	setxv : function(xv){ xvel = xv; },
+	setyv : function(yv){ yvel = yv; }
     }
 };
 
 /*------------------------- Making the Balls Bounce -------------------------*/
 
 var bounceBall = function(){
+    console.log("bounceBall");
+    //Instantiate a new ball with random radius and x/y components 
     var rad = getRandomInt(2,5)*5;
-    var bouncer = ball(getRandomInt(rad, c.width-rad), 
+    var b = ball(getRandomInt(rad, c.width-rad), 
 		       getRandomInt(rad, c.height-rad), rad); 
+
+    //Also generate a random color to spice things up
+    var red = getRandomInt(0,255);
+    var green = getRandomInt(0,255);
+    var blue = getRandomInt(0,255);
+    
+    //In here is where the animation happens
+    var bouncy = function(){
+	console.log("bouncy");
+	//Clear the square where the ball used to be 
+	//-1's and +2's because otherwise it doesn't cover the whole ball
+	ctx.clearRect(b.getx()-rad-1, b.gety()-rad-1, rad*2+2, rad*2+2);
+	
+	//Increment the ball's position
+	b.incx(b.getxv()); 
+	b.incy(b.getyv());
+
+	//Check collision with borders or other balls
+	//Border check
+	if(b.getx() < rad || b.getx() > c.width - rad){
+	    b.setxv(-1*b.getxv());
+	}
+	if(b.gety() < rad || b.gety() > c.height - rad){
+	    b.setyv(-1*b.getyv());
+	}
+
+	//Actually draw the ball 
+	ctx.fillStyle = "rgb("+red+","+green+","+blue+")";
+	ctx.beginPath();
+	ctx.arc(b.getx(),b.gety(),b.getr(),0,2*Math.PI);
+	ctx.stroke();
+	ctx.fill();
+	
+	//Call the function again
+	window.requestAnimationFrame(bouncy);
+    };
+    bouncy();
 };
+
+/*---------- Helper Functions ----------*/
+
+//Border check - checks if ball is bumping into border of canvas
+var borderCheck = function(b){
+   
+};
+
+//Link "Add Ball" button to ball creation function
+var addbtn = document.getElementById("balls");
+addbtn.addEventListener("click", bounceBall);
